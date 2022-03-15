542c8f9482b32910ce799fbfb70651aed9527717
JGGG <giovani_ogi@hotmail.com>
2022-03-15 07:31:36 -0500

    Multithreading in C# NET Part 7

---
+++ 38Raju_DemoThread/38Raju_DemoThread/threadPerformance.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace _38Raju_DemoThread
+{
+    class threadPerformance
+    {
+        public static void IncrementCounter1()
+        {
+            long Count = 0;
+            for (long i = 0; i <= 10000000; i++)
+                Count++;
+            Console.WriteLine("IncrementCounter1: "+Count);
+        }
+
+        public static void IncrementCounter2()
+        {
+            long Count = 0;
+            for (long i = 0; i <= 10000000; i++)
+                Count++;
+            Console.WriteLine("IncrementCounter2: " + Count);
+        }
+
+        static void Main()
+        {
+
+            // without threads
+            Stopwatch s1 = new Stopwatch();
+            s1.Start();
+            IncrementCounter1();
+            IncrementCounter2();
+            s1.Stop();
+
+            Console.WriteLine("Tiempo empleado contando in single threaded:"+ s1.ElapsedMilliseconds);
+            //Console.ReadLine();
+
+            // with threads
+            Thread t1 = new Thread(IncrementCounter1);
+            Thread t2 = new Thread(IncrementCounter2);
+                       
+            Stopwatch s2 = new Stopwatch();
+            
+
+            s2.Start();
+            t1.Start();
+            t2.Start();
+            s2.Stop();
+
+            t1.Join();
+            t2.Join();
+             
+            Console.WriteLine("Tiempo empleado contando in multiple threaded:" + s2.ElapsedMilliseconds);
+            Console.ReadLine();
+
+        }
+
+    }
+}
